0f1f350f95103d23c940e0f6a6eb38b936900846
parth7166 <46794689+parth7166@users.noreply.github.com>
2019-06-26 10:42:12 -0400

    Commit #2

--- rabbit.java
+++ rabbit.java
@@ -5,11 +5,15 @@
  */
 package csd3464;
 
+import com.sun.javafx.scene.traversal.Direction;
+
 /**
  *
  * @author parth
  */
 public class rabbit {
+    public int x = 0;
+    public int y = 0;
 
     /**
      * @param args the command line arguments
@@ -17,6 +21,42 @@ public class rabbit {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+
+    /**
+     *
+     * @return
+     */
+    public Direction getMove() {
+        int moves = 0;
+		moves++;
+		if (moves > 2 * getJumpHeight() + 2) {
+			moves = 1;   // start over
+		}
+		
+		if (moves <= getJumpHeight()) {   // 1st or 2nd move
+			return Direction.NORTH;
+		} else if (moves <= 2 * getJumpHeight()) {   // 3rd or 4th
+			return Direction.SOUTH;
+		} else {  // 5th or 6th
+			return Direction.EAST;
+		}
+	}
+
+
+    private int getJumpHeight() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public String toString() {
+		return "V";
+
+    private static class To {
+
+        public To() {
+        }
+    }
+    }
     
     
 }
+
